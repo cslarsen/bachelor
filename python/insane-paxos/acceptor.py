d95676a57250f4390bfa682dfcecfd60f5b0db2b
Christian Stigen <Christian.Stigen@Emerson.com>
2014-02-20 10:37:59 +0100

    Adds docstrings

--- python/insane-paxos/acceptor.py
+++ python/insane-paxos/acceptor.py
@@ -5,27 +5,33 @@ class Acceptor(PaxosRole):
   """A classic Paxos acceptor."""
   def __init__(self, ip='', port=0, proposers=[], learners=[]):
     PaxosRole.__init__(self, "Acceptor", ip, port)
-    self.proposers = proposers
-    self.learners = learners
-    self.rnd = 0 # current round number
-    self.vrnd = None # last voted round number
-    self.vval = None # value of laste voted round
+    self.proposers = proposers # (ip, port) of proposers
+    self.learners = learners # (ip, port) of learners
+    self.rnd = 0 # Current round number
+    self.vrnd = None # Last voted round number
+    self.vval = None # Value of last voted round
 
   def __repr__(self):
+    """Returns a string representation of this object, used when printing
+    it."""
     return "<{0} {1}:{2} rnd={3} vrnd={4} vval={5}>".format(
       self.name, self.udp.ip, self.udp.port,
       self.rnd, self.vrnd, self.vval)
 
   # Phase 1b
   def on_prepare(self, c, n): # c = sender
+    """Called when we receive a prepare message."""
     log.info("{0} on_prepare({1}, {2})".format(self, c, n))
 
     if n > self.rnd:
       self.rnd = n # the next round number
+
+      # Send PROMISE message back to the one who sent us a PREPARE message
       self.promise(c, self.rnd, self.vrnd, self.vval)
 
   # Phase 2b
   def on_accept(self, c, n, v): # c = sender
+    """Called when we receive an accept message."""
     log.info("{0} on_accept({1}, {2}, {3})".format(self, c, n, v))
 
     if n >= self.rnd and n != self.vrnd:
@@ -33,6 +39,7 @@ class Acceptor(PaxosRole):
       self.vrnd = n
       self.vval = v
 
+      # Send LEARN message to learners
       for L in self.learners:
         self.learn(L, n, v)
 
