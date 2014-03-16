12156d43b5c12791b89c3ee50d819cb864973ebb
Christian Stigen Larsen <csl@sublevel3.org>
2014-03-16 20:29:54 +0100

    Adds dedicated clients to network

--- paxos/topology.py
+++ paxos/topology.py
@@ -3,13 +3,30 @@ from paxos import log
 
 class SimpleTopology(Topo):
   """A Mininet topology consisting of switches and hosts."""
-  def __init__(self, count_switches=1, hosts_per_switch=3, **kw):
+  def __init__(self,
+               count_switches=1,
+               hosts_per_switch=3,
+               count_clients=1,
+               **kw):
+
     Topo.__init__(self, *kw)
+
+    link_options = {"bw": 10,
+                    "delay": "5ms",
+                    "loss": 0,
+                    "use_htb": True}
+
+    # Add client(s) first so their addresses start at 10.0.0.1 and so on.
+    clients = []
+    for n in range(count_clients):
+      client = self.addHost("cl%d" % n)
+      log.debug("Adding client {}".format(client))
+      clients.append(client)
 
     # Add switches
     switches = []
     hostno = 0
-    for n in range(0, count_switches):
+    for n in range(count_switches):
       switch = self.addSwitch("s%d" % n)
       log.debug("Adding switch {}".format(switch))
       switches.append(switch)
@@ -19,9 +36,14 @@ class SimpleTopology(Topo):
         host = self.addHost("h%d" % hostno)
         hostno += 1
         log.debug("Adding host {} to switch {}".format(host, switch))
-        self.addLink(host, switch, bw=10, delay="5ms", loss=0, use_htb=True)
+        self.addLink(host, switch, **link_options)
+
+    # Connect client(s) to first switch
+    cl0 = clients[0]
+    s0 = switches[0]
+    log.debug("Adding client {} to switch {}".format(cl0, s0))
+    self.addLink(cl0, s0, **link_options)
 
     # Add links between switches
     for sw in switches[1:]:
-      self.addLink(switches[0], sw, bw=10, delay="5ms", loss=0,
-          use_htb=True)
+      self.addLink(switches[0], sw, **link_options)
