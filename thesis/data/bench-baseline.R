2076ea3f7fc8a824b36b39169ca35e142b2d0172
Christian Stigen Larsen <csl@sublevel3.org>
2014-04-25 20:51:40 +0200

    Plot two graphs for ping RTT

--- thesis/data/bench-baseline.R
+++ thesis/data/bench-baseline.R
@@ -10,11 +10,34 @@ print_table <- function(label, caption, data) {
 }
 
 pings <- read.csv("~/bach/thesis/data/pings.csv")
+summary(pings)
 
+plotSlice <- function(x, y, left, right, xlab="x", ylab="y", type="l",...) {
+	x <- x[left:right]
+	y <- y[left:right]
+	plot(x,
+	     y,
+	     xlim=c(left, right),
+	     ylim=c(min(y), max(y)),
+	     xlab=xlab,
+	     ylab=ylab,
+	     type=type,
+	     ...)
+}
+	 
 print_table("table:bench.baseline.summary",
             "Summary of baseline benchmark",
-            summary_table)
+            pings)
         
+# Two graphs on top of each other
+par(mfrow=c(2,1))
+plotSlice(pings$Req, pings$RTT, 1, 19,
+          xlab="ICMP request no.",
+          ylab="RTT (ms)",
+          main="RTT during ramp-up")
+plotSlice(pings$Req, pings$RTT, 20, length(pings$RTT),
+          xlab="ICMP request no.",
+          ylab="RTT (ms)",
+          main="RTT after ramp-up")
 
-quantile(pings$RTT, 0.25)
-summary(pings)
+#quantile(pings$RTT, 0.25)
