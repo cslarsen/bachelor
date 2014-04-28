8ea9f6a62c63b264ae9aa3ecf2b486c5c2673b6b
Christian Stigen Larsen <csl@sublevel3.org>
2014-04-28 10:56:42 +0200

    Added sd and var

--- thesis/data/combined-summary.R
+++ thesis/data/combined-summary.R
@@ -1,15 +1,27 @@
 # Create combined data
 
+# Read data
 f <- read.csv("pings.csv")
 n <- read.csv("pings-noflows.csv")
 
+# Transpose to matrix
 sf <- t(summary(f$RTT))
 sn <- t(summary(n$RTT))
 
+# Add data
+sf <- cbind(sf, "Std.dev"=sd(f$RTT), "Var"=var(f$RTT))
+sn <- cbind(sn, "Std.dev"=sd(n$RTT), "Var"=var(n$RTT))
+
+# Set names for rows
 rownames(sf) <- "With flows"
 rownames(sn) <- "Without flows"
 
+# Combine them into one table
 s <- rbind(sf,sn)
 
+# Print as LaTeX
 library(xtable)
-print.xtable(xtable(s), comment=FALSE)
+print.xtable(xtable(s,
+             label="table:rtt.baseline.summary",
+             caption="Summary of baseline ICMP ping RTTs (ms)"),
+             comment=FALSE)
