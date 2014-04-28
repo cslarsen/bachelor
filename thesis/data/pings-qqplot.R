672a96aba8e73cb605e171188fd04d2b63abf497
Christian Stigen Larsen <csl@sublevel3.org>
2014-04-28 19:52:03 +0200

    Include qqplot for noflows

--- thesis/data/pings-qqplot.R
+++ thesis/data/pings-qqplot.R
@@ -1,7 +1,23 @@
 f <- read.csv("pings.csv")
 rf <- f$RTT
 
+f <- read.csv("~/bach/thesis/data/pings.csv")
+rf <- f$RTT
+
+n <- read.csv("~/bach/thesis/data/pings-noflows.csv")
+nf <- n$RTT
+
 # Normal fit
-pdf("pings-qqplot.pdf")
-qqnorm(rf, lwd=1) #, pch=16, cex=0.5)
-qqline(rf, lwd=1)
+pdf("~/bach/thesis/data/pings-qqplot.pdf")
+
+par(mfrow=c(1,2))
+
+qqnorm(rf, lwd=1,
+       main="Normal Q-Q Plot (with flows)")
+       #pch=16, cex=0.5
+qqline(rf, lwd=2, col="red")
+
+qqnorm(nf, lwd=1,
+       main="Normal Q-Q Plot (without flows)")
+       #pch=16, cex=0.5
+qqline(nf, lwd=2, col="red")
